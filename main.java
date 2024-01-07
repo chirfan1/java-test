cc9da91744bfd030283ee808b775296c03dc8552
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2024-01-07 15:09:37 +0700

    Java Test (test) : Creating new java repository

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 5};
+
+        for (int i:num){
+            if (i == 3){
+                System.out.println(i);
+                break;
+            }        }
+    }
+}
