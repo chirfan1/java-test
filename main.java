f21629e1561ecd49db3252979b12b36e62e4b3a3
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2024-01-07 15:27:17 +0700

    Java Test (test) : Pyramid(?)

--- main.java
+++ main.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        int[] num = {1, 2, 3, 4, 5};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan Size : ");
+        int num = sc.nextInt();
 
-        for (int i:num){
-            if (i == 3){
-                System.out.println(i);
-                break;
-            }        }
+        for (int i = 1; i<=num; i++){
+            for (int j = 1;j<num-(i-1);j++){
+                System.out.println(" ");
+            }
+            for (int k = 1; k<=i;k++){
+                System.out.print("*");
+                for (int k1 = 1;k1<k;k1+=k){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
     }
 }
